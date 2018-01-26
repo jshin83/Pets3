4bd304fe9ef8ff8815574d2edd8719c1e8f8bb85
Raine Padilla <epadilla7@mail.greenriver.edu>
2018-01-26 14:10:31 -0500

    added validString function and error checking

--- model/validate.php
+++ model/validate.php
@@ -6,8 +6,27 @@
  * Time: 11:00 AM
  */
 
+$errors = array();
+
 function validColor($color)
 {
     global $f3;
     return in_array($color, $f3->get('colors'));
 }
+
+function validString($string)
+{
+    return !empty($string) && ctype_alpha($string);
+}
+
+if(!validColor($color))
+{
+    $errors['color'] = "Please enter a valid color.";
+}
+
+if(!validString($string))
+{
+    $errors['string'] = "Please enter a valid string.";
+}
+
+$success = sizeof($errors) == 0;
